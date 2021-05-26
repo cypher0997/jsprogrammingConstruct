21c93bf71f3075a594705d9436fac290786c16ce
testForMe-Now <abcodehash@gmail.com>
2021-05-26 12:43:09 +0530

    modified and convert number in weekdays is added

---
+++ jsProgrammingConstruct/toGetWeekDays.js
@@ -0,0 +1,30 @@
+const prompt = require('prompt-sync')();
+const number = parseFloat(prompt("Enter number: "));
+
+switch(number){
+    case 1:
+        console.log("sunday")
+        break;
+    
+    case 2:
+        console.log("monday")
+        break;
+    case 3:
+        console.log("tuesday")
+        break;
+    case 4:
+        console.log("wednesday")
+        break;
+    case 5:
+        console.log("thrusday")
+        break;
+    case 6:
+        console.log("friday")
+        break;
+    case 7:
+        console.log("saturday")
+        break;
+    default:
+        console.log("wrong input")
+    
+}
